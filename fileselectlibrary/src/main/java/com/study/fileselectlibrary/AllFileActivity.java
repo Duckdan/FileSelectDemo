79f6959d83a858c3320fd8ef3abe847101e4146f
邓超@xgtl <2325690622@qq.com>
2020-01-06 15:12:39 +0800

    Migrate to androidx

--- fileselectlibrary/src/main/java/com/study/fileselectlibrary/AllFileActivity.java
+++ fileselectlibrary/src/main/java/com/study/fileselectlibrary/AllFileActivity.java
@@ -3,7 +3,7 @@ package com.study.fileselectlibrary;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.text.format.Formatter;
 import android.view.View;
 import android.widget.Button;
--- fileselectlibrary/src/main/java/com/study/fileselectlibrary/utils/PermissionCheckUtils.java
+++ fileselectlibrary/src/main/java/com/study/fileselectlibrary/utils/PermissionCheckUtils.java
@@ -4,9 +4,9 @@ import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 import java.util.List;
